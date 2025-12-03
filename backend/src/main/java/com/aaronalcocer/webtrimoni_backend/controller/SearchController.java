c0ee7e11b5a053db0c8f02655ca6b82c0698e92c
Nithaat0R <nithaator.oficial@gmail.com>
2025-12-03 22:21:15 +0100

    03/12/2025

--- backend/src/main/java/com/aaronalcocer/webtrimoni_backend/controller/SearchController.java
+++ backend/src/main/java/com/aaronalcocer/webtrimoni_backend/controller/SearchController.java
@@ -3,6 +3,8 @@ package com.aaronalcocer.webtrimoni_backend.controller;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -17,11 +19,89 @@ public class SearchController {
 
     //Generem un endpoint
     @GetMapping("/api/search")
-    public String getFirstThreeElements(@RequestParam String search) {
+    public ArrayNode getElementsBySearch(@RequestParam String search) {
         try {
-            return search;
+
+            ArrayNode result = (ArrayNode) getElementsByTitle(search);
+            ArrayNode patrimoniPerMunicipi = (ArrayNode) getElementsByTown(search).get("elements");
+
+            for (JsonNode elem : patrimoniPerMunicipi){
+
+            }
+
+
+            ArrayNode patrimoniPerDescripcio = (ArrayNode) getElementsByDescripton(search).get("elements");
+
+            
+
+            return result;
+
         } catch (Exception e) {
-            return "Error";
+            e.printStackTrace();
+            return objectMapper.createArrayNode();
+        }
+    }
+
+    public ArrayNode getElementsByTitle(String search) {
+        //Definim la URL de l'API 
+        String url = "https://do.diba.cat/api/dataset/patrimoni_cultural/ord-titol/asc/camp-titol-like/" + search;
+
+        try {
+
+            //Agafem el JSON en format string
+            String jsonString = restTemplate.getForObject(url, String.class);
+            //Generem un arbre de nodes a partir del JSON per poder manipular facilment
+            JsonNode root = objectMapper.readTree(jsonString);
+            //Accedim a elements ja que la resta del JSON és informació de l'API
+            ArrayNode elements = (ArrayNode) root.path("elements");
+
+            return elements;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return objectMapper.createArrayNode();
+        }
+    }
+
+    public ArrayNode getElementsByTown(String search) {
+        //Definim la URL de l'API 
+        String url = "https://do.diba.cat/api/dataset/patrimoni_cultural/ord-titol/asc/camp-municipi_nom-like/" + search;
+
+        try {
+
+            //Agafem el JSON en format string
+            String jsonString = restTemplate.getForObject(url, String.class);
+            //Generm un arbre de nodes a partir del JSON per poder manipular facilment
+            JsonNode root = objectMapper.readTree(jsonString);
+            //Accedim a elements ja que la resta del JSON és informació de l'API
+            ArrayNode elements = (ArrayNode) root.path("elements");
+
+            return elements;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return objectMapper.createArrayNode();
+        }
+    }
+    
+    public ArrayNode getElementsByDescripton(String search) {
+        //Definim la URL de l'API 
+        String url = "https://do.diba.cat/api/dataset/patrimoni_cultural/ord-titol/asc/camp-descripcio-like/" + search;
+
+        try {
+
+            //Agafem el JSON en format string
+            String jsonString = restTemplate.getForObject(url, String.class);
+            //Generm un arbre de nodes a partir del JSON per poder manipular facilment
+            JsonNode root = objectMapper.readTree(jsonString);
+            //Accedim a elements ja que la resta del JSON és informació de l'API
+            ArrayNode elements = (ArrayNode) root.path("elements");
+
+            return elements;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return objectMapper.createArrayNode();
         }
     }
 }
