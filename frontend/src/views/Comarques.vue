c6ad69932d73ceca81e810488000d04048a4c3b2
Nithaat0R <nithaator.oficial@gmail.com>
2025-12-17 18:28:45 +0100

    Imatges afegides

---
+++ frontend/src/views/Comarques.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="resultats">
+    <h1>Resultats per "{{ query }}"</h1>
+
+    <div v-if="loading">Cercant...</div>
+
+    <div v-else>
+      <div v-if="resultats.length === 0">
+        No s'han trobat resultats
+      </div>
+
+      <div class="cards">
+        <div @click="go(el.id)" class="card" v-for="el in resultats" :key="el.id">
+          <h3>{{ el.titol }}</h3>
+          <a :href="el.url" target="_blank">Veure fitxa</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Resultats",
+  data() {
+    return {
+      query: "",
+      resultats: [],
+      loading: false,
+    };
+  },
+  mounted() {
+    this.query = this.$route.query.q;
+    if (this.query) {
+      this.cerca();
+    }
+  },
+  methods: {
+    async cerca() {
+      this.loading = true;
+      try {
+        const response = await axios.get("http://localhost:8080/api/search", {
+          params: { search: this.query },
+        });
+        this.resultats = response.data.elements ?? response.data;
+      } catch (e) {
+        console.error(e);
+      } finally {
+        this.loading = false;
+      }
+    },
+    go(id) {
+    console.log("CLICK EN:", id);
+    this.$router.push({
+      name: "patrimoni",
+      query: { id }
+    });
+  }
+  },
+};
+</script>
+
+<style>
+.cards {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
+  gap: 16px;
+}
+.card {
+  background: white;
+  padding: 16px;
+  border-radius: 8px;
+  border: 1px solid #ddd;
+}
+</style>
--- frontend/src/views/InfoPatrimoni.vue
+++ frontend/src/views/InfoPatrimoni.vue
@@ -2,9 +2,14 @@
   <div class="info-wrapper" v-if="element">
     <div class="card">
 
-      <div class="image">
-        <div class="placeholder">Imatge</div>
-      </div>
+    <div class="image">
+      <img
+        v-if="primeraImatge"
+        :src="primeraImatge"
+        alt="Imatge del patrimoni"
+      />
+  <div v-else class="placeholder">Sense imatge</div>
+</div>
 
       <div class="data">
         <h2>{{ element.titol }}</h2>
@@ -54,6 +59,15 @@ export default {
       console.error("Error carregant patrimoni", e);
     }
   },
+  computed: {
+  primeraImatge() {
+    if (!this.element) return null;
+    if (!this.element.images) return null;
+    if (!this.element.images.length) return null;
+
+    return this.element.images[0].split("|")[0];
+  }
+}
 };
 </script>
 
